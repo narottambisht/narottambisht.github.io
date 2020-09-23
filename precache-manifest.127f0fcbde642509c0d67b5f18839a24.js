1e7a27d6d558b80598bfd82be3fdde79d7e633a7
narottambisht <bishtnarottam@gmail.com>
2020-09-24 04:28:18 +0530

    Updates

---
+++ precache-manifest.127f0fcbde642509c0d67b5f18839a24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4e078c43e47acc95db06ca7d5b7cd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1fe310fb397cefe09ee",
+    "url": "/static/css/main.a1270b51.chunk.css"
+  },
+  {
+    "revision": "6ea0768a6f0c100e934d",
+    "url": "/static/js/2.4a70938f.chunk.js"
+  },
+  {
+    "revision": "014c8388aa3aa7d38ce44459649a48eb",
+    "url": "/static/js/2.4a70938f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1fe310fb397cefe09ee",
+    "url": "/static/js/main.397ecd24.chunk.js"
+  },
+  {
+    "revision": "ed68b692941a88f46b10",
+    "url": "/static/js/runtime-main.dd3a324e.js"
+  }
+]);
