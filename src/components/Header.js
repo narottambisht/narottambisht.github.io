ae7edeec0dd684f15e27867630926f5630e2b4bb
Narottam Singh Bisht <bishtnarottam@gmail.com>
2023-04-25 01:11:00 +0530

    test jenkins 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -91,7 +91,7 @@ const Header = props => {
           </Grid>
           <Grid item lg={5} md={4} sm={12} className={classes.headerIntro}>
             <Typography variant="h4" className={classes.profileIntroName}>
-              Hey 👋 Welcome noddy bhai
+              Hey 👋 Welcome
               <span role="img" aria-label="welcome-emoji"> 🤓</span>,
               <br/>
               I'm <strong>{remoteConfigStore.name}</strong>
