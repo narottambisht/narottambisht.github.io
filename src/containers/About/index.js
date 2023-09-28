0d01137ff34a7b1171aa73299c8b135a9791e92d
narottambisht <bishtnarottam@gmail.com>
2023-09-29 04:08:02 +0530

    fix

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState }                   from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Card, CardContent, CardHeader, Divider, Grid, Typography } from "@material-ui/core";
-import aboutStyles                                                  from "./styles";
-import ImageGrid, { ImageGridModal }                                from "../../components/ImageGrid"
-import { firestoreDB }                                              from "../../utils/FirebaseConfig";
-import { AboutContext }                                             from "../../context/AboutContext";
+import aboutStyles from "./styles";
+import ImageGrid, { ImageGridModal } from "../../components/ImageGrid"
+import { firestoreDB } from "../../utils/FirebaseConfig";
+import { AboutContext } from "../../context/AboutContext";
 
 const About = () => {
   const [selectedImg, setSelectedImg] = useState(null);
@@ -23,43 +23,43 @@ const About = () => {
         <Grid item lg={8} sm={12}>
           <Grid item lg={12} sm={12} style={{ marginBottom: 15 }}>
             <Card>
-              <CardHeader title="ABOUT ME"/>
-              <Divider/>
+              <CardHeader title="ABOUT ME" />
+              <Divider />
               <CardContent>
-                {aboutStore.about_me}
+                <p dangerouslySetInnerHTML={{ __html: aboutStore.about_me }} />
               </CardContent>
             </Card>
           </Grid>
           <Grid item lg={12} sm={12} style={{ marginBottom: 15 }}>
             <Card>
-              <CardHeader title="FUTURE GOALS"/>
-              <Divider/>
+              <CardHeader title="FUTURE GOALS" />
+              <Divider />
               <CardContent>
                 {aboutStore.future_goals &&
-                aboutStore.future_goals.goals_array.map((_goal, index) => {
-                  return (
-                    <React.Fragment key={index}>
-                      <Typography variant="subtitle1" paragraph={true}
-                                  style={{ fontWeight: "bold" }}>
-                        {_goal.goal_title}
-                      </Typography>
-                      <Typography variant="subtitle2" component="div" gutterBottom={true}>
-                        {_goal.goal_description}
-                      </Typography>
-                    </React.Fragment>
-                  )
-                })}
+                  aboutStore.future_goals.goals_array.map((_goal, index) => {
+                    return (
+                      <React.Fragment key={index}>
+                        <Typography variant="subtitle1" paragraph={true}
+                          style={{ fontWeight: "bold" }}>
+                          {_goal.goal_title}
+                        </Typography>
+                        <Typography variant="subtitle2" component="div" gutterBottom={true}>
+                          {_goal.goal_description}
+                        </Typography>
+                      </React.Fragment>
+                    )
+                  })}
               </CardContent>
             </Card>
           </Grid>
           <Grid item lg={12} sm={12}>
             <Card>
-              <CardHeader title="ACHIEVEMENTS"/>
-              <Divider/>
+              <CardHeader title="ACHIEVEMENTS" />
+              <Divider />
               <CardContent>
-                <ImageGrid setSelectedImg={setSelectedImg} imageArray={aboutStore.achievements}/>
+                <ImageGrid setSelectedImg={setSelectedImg} imageArray={aboutStore.achievements} />
                 {selectedImg && (
-                  <ImageGridModal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/>
+                  <ImageGridModal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
                 )}
               </CardContent>
             </Card>
@@ -68,19 +68,19 @@ const About = () => {
         <Grid item lg={4} sm={12} xs={12}>
           <Grid item lg={12} sm={12} xs={12} style={{ marginBottom: 15 }}>
             <Card>
-              <CardHeader title="DETAILS"/>
-              <Divider/>
+              <CardHeader title="DETAILS" />
+              <Divider />
               <CardContent>
                 <div style={{ marginBottom: 10 }} className={classes.aboutDetails}>
                   <div style={{ fontWeight: "bold" }}>Date of birth</div>
                   <div>12th June, 1995</div>
                 </div>
-                <Divider/>
+                <Divider />
                 <div style={{ marginBottom: 10, marginTop: 10 }} className={classes.aboutDetails}>
                   <div style={{ fontWeight: "bold" }}>Nationality</div>
                   <div>Indian</div>
                 </div>
-                <Divider/>
+                <Divider />
                 <div style={{ marginTop: 10 }} className={classes.aboutDetails}>
                   <div style={{ fontWeight: "bold" }}>Marrital Status</div>
                   <div>Unmarried</div>
@@ -90,19 +90,19 @@ const About = () => {
           </Grid>
           <Grid item lg={12} sm={12} xs={12}>
             <Card>
-              <CardHeader title="HOBBIES"/>
-              <Divider/>
+              <CardHeader title="HOBBIES" />
+              <Divider />
               <CardContent>
                 {aboutStore.hobbies && aboutStore.hobbies.map((_hobby, index) => {
                   return (
                     <>
                       <div className={classes.hobbyDiv} key={index}>
-                        <img src={_hobby.hobby_logo_url} height={40} width={40} alt="hobby-logow"/>
+                        <img src={_hobby.hobby_logo_url} height={40} width={40} alt="hobby-logow" />
                         <Typography variant="subtitle1">
                           {_hobby.hobby_name}
                         </Typography>
                       </div>
-                      {index + 1 !== aboutStore.hobbies.length && <Divider/>}
+                      {index + 1 !== aboutStore.hobbies.length && <Divider />}
                     </>
                   )
                 })}
